72a7dff25e3e271693f8838ef4c0112c551de8e9
masumkawser <kawser151722@gmail.com>
2022-09-03 15:14:35 +0600

    Dynamic Card added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,49 @@
 
-const loadAllcatagory = async() =>{
+// All news 
+const url='https://openapi.programming-hero.com/api/news/categories'
+fetch(url)
+.then(req=>req.json())
+.then(allnews=>allNewsCata(allnews.data.news_category))
 
-   const response =await fetch("https://openapi.programming-hero.com/api/news/categories") ;
-
-   const data = await response.json();
-
-   return data ;
+const allNewsCata = allnews=>{
+  for(let eachNewsCat of allnews){
+    const ul1=document.getElementById('ul1');
+    const li=document.createElement('li');
+    li.classList.add('nav-item');
+    li.innerHTML=`
+    <a class="nav-link" href="#" id="${eachNewsCat.category_id}" onclick="eachButton('${eachNewsCat.category_id}')">${eachNewsCat.category_name}</a>
+    `
+    ul1.appendChild(li);
+  }
+}
+function eachButton(id){
+  console.log(id)
+  const idurl=`https://openapi.programming-hero.com/api/news/category/${id}`
+  fetch(idurl)
+  .then(req1=>req1.json())
+  .then(eachNews=>eachNewsPortal(eachNews.data))
 }
 
-console.log( loadAllcatagory());
+const eachNewsPortal= eachNews=>{
+  const newShow=document.getElementById('newShow');
+  newShow.innerHTML='';
+  for(let eachNewshow of eachNews){
+    const div=document.createElement('div');
+    div.classList.add('row');
+    div.innerHTML=`
+    <div  class="card mb-3 mt-5 border border" style="max-width:100%;">
+    <div class="col-md-4">
+    <img src="${eachNewshow.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">${eachNewshow.title}</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  </div>`
+      
+      newShow.appendChild(div);
+  }
+}
