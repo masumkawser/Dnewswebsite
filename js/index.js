b9354e269f828b0448b27ffe04edd21520f0d635
masumkawser <kawser151722@gmail.com>
2022-09-03 08:20:46 +0600

    api url cheacked

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 
-console.log('data loaded');
+const loadAllcatagory = async() =>{
+
+   const response =await fetch("https://openapi.programming-hero.com/api/news/categories") ;
+
+   const data = await response.json();
+
+   return data ;
+}
+
+console.log( loadAllcatagory());
