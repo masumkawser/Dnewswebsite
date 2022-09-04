3e9567ff78fbca0e14850c4888a5180046f04508
masumkawser <kawser151722@gmail.com>
2022-09-04 08:02:48 +0600

    sort news

--- js/index.js
+++ js/index.js
@@ -1,51 +1,197 @@
+.
 
-// All news 
-const url='https://openapi.programming-hero.com/api/news/categories'
-fetch(url)
-.then(req=>req.json())
-.then(allnews=>allNewsCata(allnews.data.news_category))
-
-const allNewsCata = allnews=>{
-  for(let eachNewsCat of allnews){
-    const ul1=document.getElementById('ul1');
-    const li=document.createElement('li');
-    li.classList.add('nav-item');
-    li.innerHTML=`
-    <a class="nav-link" href="#" id="${eachNewsCat.category_id}" onclick="eachButton('${eachNewsCat.category_id}')">${eachNewsCat.category_name}</a>
-    `
-    ul1.appendChild(li);
+const showAllCategories = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  try {
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data.data[0]);
+      displayAllCategories(data.data.news_category);
+  } catch (error) {
+      console.log(error);
   }
-}
-function eachButton(id){
-  console.log(id)
-  const idurl=`https://openapi.programming-hero.com/api/news/category/${id}`
-  fetch(idurl)
-  .then(req1=>req1.json())
-  .then(eachNews=>eachNewsPortal(eachNews.data))
-}
+};
 
-const eachNewsPortal= eachNews=>{
-  const newShow=document.getElementById('newShow');
-  newShow.innerHTML='';
-  for(let eachNewshow of eachNews){
-    const div=document.createElement('div');
-    div.classList.add('row');
-    div.innerHTML=`
-    <div  class="card mb-3 mt-5 border border" style="max-width:100%;">
-    <div class="col-md-4">
-    <img src="${eachNewshow.thumbnail_url}" class="img-fluid rounded-start" alt="...">
-  </div>
-  <div class="col-md-8">
-    <div class="card-body">
-      <h5 class="card-title">${eachNewshow.title}</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    //   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    //   Detail more...
-    // </button>
-    </div>
-  </div>
-  </div>`
+displayAllCategories = (categories) => {
+  console.log(categories);
+  const newsContainer = document.getElementById('news-categories');
+  categories.forEach((category) => {
+      const li = document.createElement('li');
+      li.classList.add('active-color')
+      li.innerHTML = `
+
+  <a onclick="loadAllNews('${category.category_id ? category.category_id : 'Not found'
+          }')" class=" text-decoration-none text-dark d-inline-block  px-3  active border-0 " href="#">${category.category_name ? category.category_name : 'Not found'
+          }</a>
+  `;
+
+      newsContainer.appendChild(li);
+  });
+};
+showAllCategories();
+
+
+const loadAllNews = async (id) => {
+  toggleSpinner(true);
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  try {
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data.data[0]);
+      displayAllNews(data.data, id);
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+const displayAllNews = (allNews, cId) => {
+ 
+
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.textContent = '';
+ 
+  const noNewsFound = document.getElementById('not-found');
+  if (allNews.length == 0) {
+      noNewsFound.classList.remove('d-none');
+  } else {
+      noNewsFound.classList.add('d-none');
+  }
+  allNews.sort((a, b) => {
+      return b.total_view - a.total_view;
+  });
+  allNews.forEach((news) => {
       
-      newShow.appendChild(div);
+      const newsDiv = document.createElement('div');
+      newsDiv.classList.add('col');
+      newsDiv.innerHTML = `
+      <div class="row g-3 shadow p-2 rounded">
+          <div class="card col-md-3 col-lg-3 col-12 border-0">
+              <img class="img-fluid pb-3" src="${news.thumbnail_url ? news.thumbnail_url : 'Not found'}" class="card-img-top" alt="...">
+          </div>
+          <div class="card-body col-md-9 col-lg-9 col-12 align-itmes-center">
+              <div class="ps-md-5 ps-sm-3 ps-3 pt-3">
+                  <h5 class="card-title fs-4 fs-md-4 fw-bolder">${news.title ? news.title : 'Not Found'}</h5>
+                  <p class="card-text my-3">${news.details.slice(0, 350)}...</p>
+                  <a onclick="newsModal('${news._id}')" type="button" class=" mb-3 text-decoration-none text-danger custom-link" data-bs-toggle="modal" data-bs-target="#exampleModal">Read More <i class="fa-solid fa-arrow-right"></i></a>
+
+                  <div class="row justify-content-between align-items-center mt-4 pb-3">
+
+                      <div class="col-6 d-flex justify-content-start align-baseline align-items-center">
+                      <div>
+                          <img class="rounded-5" style="width:50px; height: 50px" src="${news.author.img ? news.author.img : 'Not found'}" alt="">
+                      </div>
+                      <div>
+                          <span class="ps-2 text-primary fw-bolder  text-capitalize">${news.author.name ? news.author.name : 'Not found'
+                          }</span> <br>
+                          <span class="ps-2">${news.author.published_date ? news.author.published_date : 'Not found'
+                          }</span>
+                      </div>
+                      </div>
+                      <div class="col-6">
+                      <div class="d-flex justify-content-around align-baseline align-items-center">
+                          <li class="list-unstyled me-3">
+                              <i class="fa-regular fa-eye text-primary"></i> ${news.total_view ? news.total_view : 'Not found'
+                              }
+                          </li>
+                          <li class="list-unstyled me-3">
+                          <i class="fa-solid fa-star text-warning"></i> ${news.rating.number ? news.rating.number : 'Not found'
+                              }
+                          </li>
+                      </div>
+                      </div>
+                  </div>
+              </div> 
+          </div> 
+      </div>      
+      `;
+      newsContainer.appendChild(newsDiv);
+  });
+  toggleSpinner(false);
+
+ 
+  const newsDiv = [];
+  const totalNews = (document.getElementById('news-count').innerText =
+      allNews.length);
+  for (let i = 0; i > 0; i++) {
+      const name = newsDiv[i].totalNews;
+      const span = document.createElement('span');
+      span.innerHTML = `
+<p>${newsDiv}</p>
+`;
+      totalNews.appendChild(name);
   }
-}
+
+ 
+  const categoryName = async () => {
+      const url = `https://openapi.programming-hero.com/api/news/categories`;
+      try {
+          const res = await fetch(url);
+          const data = await res.json();
+        
+          displayCategory(data.data.news_category);
+      } catch (error) {
+          console.log(error);
+      }
+  };
+  const displayCategory = (news) => {
+      const categoryName = document.getElementById('category-name');
+      categoryName.innerText = news[cId - 1].category_name;
+  };
+
+  categoryName();
+};
+
+
+const toggleSpinner = (isLoading) => {
+  const loaderSpinner = document.getElementById('News-spinner');
+  if (isLoading) {
+      loaderSpinner.classList.remove('d-none');
+  } else {
+      loaderSpinner.classList.add('d-none');
+  }
+};
+
+
+const newsModal = async (_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${_id}`;
+  try {
+      const res = await fetch(url);
+      const data = await res.json();
+     
+      modalDetails(data.data[0]);
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+const modalDetails = (modalNews) => {
+  
+  const modalTitle = document.getElementById('exampleModalLabel');
+  modalTitle.innerText = modalNews.title ? modalNews.title : 'Not found';
+  const modalDetailsContainer = document.getElementById('news-modal');
+  modalDetailsContainer.textContent = '';
+  const modalDiv = document.createElement('div');
+  modalDiv.innerHTML = `
+  
+  <img src="${modalNews.image_url ? modalNews.image_url : 'Not found'
+      }" class="card-img-top" alt="...">
+  <div class="card-body">
+      <h5 class="card-title">Name: ${modalNews.author.name ? modalNews.author.name : 'Not found'
+      }</h5>
+      <p class="card-title"><b>Published Date:</b> ${modalNews.author.published_date
+          ? modalNews.author.published_date
+          : 'Not found'
+      }</p>
+      <p class="card-title"><b>Details:</b> ${modalNews.details ? modalNews.details : 'Not found'
+      }</p>
+      <p class="card-text"><b>Rating:</b> ${modalNews.rating.number ? modalNews.rating.number : 'Not found'
+      }</p>
+      <p class="card-text"><b>Views:</b> ${modalNews.total_view ? modalNews.total_view : 'Not found'
+      }</p>
+  </div>
+  
+  `;
+  modalDetailsContainer.appendChild(modalDiv);
+};
+
+loadAllNews('08')
